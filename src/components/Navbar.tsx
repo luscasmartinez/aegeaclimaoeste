06669e8a120c009cfa126adfb6a72039c2500eda
Lucas de Lima Martinez <106637076+luscasmartinez@users.noreply.github.com>
2026-01-30 22:20:29 -0300

    Alterado nomes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ export function Navbar() {
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="flex items-center space-x-2 text-blue-600 hover:text-blue-700 transition">
             <Cloud className="w-8 h-8" />
-            <span className="font-bold text-xl">MeteoInfo</span>
+            <span className="font-bold text-xl">Clima Oeste</span>
           </Link>
 
           <div className="flex items-center space-x-2 md:space-x-4">
